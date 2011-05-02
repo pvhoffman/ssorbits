737468452d49b7ee26367ee81d732c7d95c25fe6
Paul Hoffman <pvhoffman@gmail.com>
2011-05-02 14:20:42 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+from sun import View as SV
+
+application = webapp.WSGIApplication(
+        [('/', SV)
+            , ('/sun', SV)],
+        debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
+
