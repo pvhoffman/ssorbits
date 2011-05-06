fa6fa6da7b8d0644f2a3fcd99c53cdd8cd246d57
Paul Hoffman <pvhoffman@gmail.com>
2011-05-06 08:57:04 -0700

    regular commit

--- main.py
+++ main.py
@@ -2,16 +2,32 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 
-from sun import View as SV
+from sun import View as sunv
+from mercury import View as mercuryv
+from venus import View as venusv
+from moon import View as earthmoonv
+from mars import View as marsv
+from jupiter import View as jupiterv
+from saturn import View as saturnv
+from uranus import View as uranusv
+from neptune import View as neptunev
 
 application = webapp.WSGIApplication(
-        [('/', SV)
-            , ('/sun', SV)],
-        debug=True)
+		[('/', sunv)
+			, ('/sun', sunv)
+			, ('/mercury', mercuryv)
+			, ('/venus', venusv)
+			, ('/moon', earthmoonv)
+			, ('/mars', marsv)
+			, ('/neptune', neptunev)
+			, ('/uranus', uranusv)
+			, ('/saturn', saturnv)
+			, ('/jupiter', jupiterv)]
+		, debug=True)
 
 def main():
-    run_wsgi_app(application)
+	run_wsgi_app(application)
 
 if __name__ == "__main__":
-    main()
+	main()
 
