e4698465bbdb31f89444a40152a02827b91c938c
Paul Hoffman <pvhoffman@gmail.com>
2011-05-13 13:55:36 -0700

    merging distributed versions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
@@ -12,22 +14,30 @@ from saturn import View as saturnv
 from uranus import View as uranusv
 from neptune import View as neptunev
 
+class googleauthv(webapp.RequestHandler):
+    def get(self):
+        (path, filename) = os.path.split(__file__)
+        fn = os.path.join(path, 'googlebc8690495dcb0c72.html')
+        fd = open(fn, 'rb');
+        self.response.out.write(fd.read())
+
 application = webapp.WSGIApplication(
-		[('/', sunv)
-			, ('/sun', sunv)
-			, ('/mercury', mercuryv)
-			, ('/venus', venusv)
-			, ('/moon', earthmoonv)
-			, ('/mars', marsv)
-			, ('/neptune', neptunev)
-			, ('/uranus', uranusv)
-			, ('/saturn', saturnv)
-			, ('/jupiter', jupiterv)]
-		, debug=True)
+        [('/', sunv)
+            , ('/sun', sunv)
+            , ('/mercury', mercuryv)
+            , ('/venus', venusv)
+            , ('/moon', earthmoonv)
+            , ('/mars', marsv)
+            , ('/neptune', neptunev)
+            , ('/uranus', uranusv)
+            , ('/saturn', saturnv)
+            , ('/jupiter', jupiterv)
+            , ('/googlebc8690495dcb0c72.html', googleauthv)]
+        , debug=True)
 
 def main():
-	run_wsgi_app(application)
+    run_wsgi_app(application)
 
 if __name__ == "__main__":
-	main()
+    main()
 
